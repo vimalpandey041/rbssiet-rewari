4baabca3f16b53dfae571fce0f634be7caa3d900
vimalpandey041 <vimalpandey041@gmail.com>
2022-10-21 16:27:12 +0530

    new

---
+++ footer.php
@@ -0,0 +1,239 @@
+<footer>
+      <div class="container-fluid gradient11" >
+        <div class="row">
+          <div class="col-12 cl-md-6 col-lg-3 " >
+           <div class="text-center mt-3">
+            <img src="/img/rbssiet new logo.png " class="img-fluid" width="110px" alt="">
+           </div>
+           <div class="row mt-3">
+            <h3 class="h5 text-white text-center ">
+              Rao Birender Singh State Institute  <br> Of Engineering And Technology, <br> Zainabad, Rewari
+            </h3 >
+           </div>
+           <div class="row mt-3 text-white flex ">
+             <div class="col-2 ">
+               <i class="fa-solid fa-phone px-3"></i>
+              </div>
+              <div class="col-10">
+                <a href="tel: +91987654321" class="nav-link">+91 987654321</a>
+              </div>
+            </div>
+            <div class="row mt-3 text-white flex ">
+             <div class="col-2 justify-content-end ">
+               <i class="fa-solid fa-phone px-3"></i>
+             </div>
+             <div class="col-10">
+               <a href="mailto:support@rbssietrewari.ac.in" class="nav-link">support@rbssietrewari.ac.in</a>
+             </div>
+            </div>
+            <div class="row my-4 text-white flex text-center ">
+              <div class="col" >
+                <i class="fa-brands fa-facebook fa-xl px-2"></i>
+        <i class="fa-brands fa-twitter fa-xl px-2"></i>
+        <i class="fa-brands fa-instagram fa-xl px-2"></i>
+        <i class="fa-brands fa-linkedin fa-xl px-2"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 cl-md-6 col-lg-3" >
+            <h4 class="text-center h2 text-white mt-3">Quick Links</h4>
+            <ul class="mt-3">
+              <li class="py-2  " style="list-style:none ;"><a href="www.techadmissionshry.gov.in" class="text-decoration-none h6 text-white">www.techadmissionshry.gov.in</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.hstes.org.in" class="text-decoration-none h6 text-white">www.hstes.org.in</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.techeduhry.gov.in" class="text-decoration-none h6 text-white">www.techeduhry.gov.in</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.aicte-india.org" class="text-decoration-none h6 text-white">www.aicte-india.org</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="https://igu.ac.in/2021/" class="text-decoration-none h6 text-white">https://igu.ac.in/2021/</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="https://pandeywebsolution.in" class="text-decoration-none h6 text-white">https://pandeywebsolution.in</a></li>
+            </ul>
+          </div>
+          <div class="col-12 cl-md-6 col-lg-3" >
+            <h4 class="text-center h2 text-white mt-3">Explore</h4>
+            <ul class="mt-3">
+              <li class="py-2  " style="list-style:none ;"><a href="www.techadmissionshry.gov.in" class="text-decoration-none h6 text-white ">About Us</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.hstes.org.in" class="text-decoration-none h6 text-white">Contact Us</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.techeduhry.gov.in" class="text-decoration-none h6 text-white">Departments</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="www.aicte-india.org" class="text-decoration-none h6 text-white">students</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="https://igu.ac.in/2021/" class="text-decoration-none h6 text-white">Faculty</a></li>
+              <li class="py-2  " style="list-style:none ;"><a href="https://pandeywebsolution.in" class="text-decoration-none h6 text-white">Route To The Institution</a></li>
+            </ul>
+          </div>
+          <div class="col-12 cl-md-6 col-lg-3" >
+            <h4 class="text-center h2 text-white mt-3">Fees Submit</h4>
+            <div class="row my-4 mx-5">
+              <div class="col rounded-4 bg-warning flex justify-content-center align-items-center " style="height: 100px;">
+                <h4 class="text-dark text-center my-2">College Fees</h4>
+              <div class="text-center my-2">
+                <button type="button" class="btn btn-dark">Submit Now</button>
+              </div>
+              </div>
+            </div>
+            <div class="row my-2 mx-5">
+              <div class="col rounded-4 bg-primary flex justify-content-center align-items-center " style="height: 100px;">
+                <h4 class="text-white text-center my-2">Hostel Fees</h4>
+              <div class="text-center my-2">
+                <button type="button" class="btn btn-warning">Submit Now</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+		</footer>
+    <footer class="text-center bg-dark text-light fw-bold">
+      <div class="text-center py-2" style="background-color: rgb(81 80 110 / 90%);">
+        Copyright   © 2022
+        <a class="text-reset  text-decoration-none" style="" href="http://rbssietrewari.ac.in/">Rbssietrewari.ac.in</a>
+        <span>||</span>
+        <span>Designed By</span>
+        <a class="text-white text-decoration-none" href="https://pandeywebsolution.in">Vimal Pandey</a>
+        <span>& Muskan Panwar</span>
+      </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+          },
+        },
+      });
+    </script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'>
+</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/owlcarousel/jquery.min.js"></script>
+<script src="/owlcarousel/owl.carousel.min.js"></script>
+<script src="/pandey.js"></script>  
+<script>
+  window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  })
+  </script>
+<script>
+  $('.owl-carousel').owlCarousel({
+
+   margin:10,
+   loop:true,
+   autoWidth:true,
+   items:4,
+   autoplay:true,
+   lazyLoad: true,
+   autoplayTimeout:3000,
+   autoplayHoverPause:true
+})
+</script>
+<script>
+  const $ = document;
+let img_btn = $.querySelectorAll(".img1");
+let items = $.querySelectorAll(".item");
+
+img_btn.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item.classList.toggle("act_img");
+    e.target.parentElement.parentElement.classList.toggle("active1");
+  });
+});
+</script>
+
+<script>
+  jQuery.fn.liScroll = function(settings) {
+	settings = jQuery.extend({
+		travelocity: 0.03
+		}, settings);		
+		return this.each(function(){
+				var $strip = jQuery(this);
+				$strip.addClass("newsticker")
+				var stripHeight = 1;
+				$strip.find("li").each(function(i){
+					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+				});
+				var $mask = $strip.wrap("<div class='mask'></div>");
+				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+				$strip.height(stripHeight);			
+				var totalTravel = stripHeight;
+				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+				function scrollnews(spazio, tempo){
+				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
+				}
+				scrollnews(totalTravel, defTiming);				
+				$strip.hover(function(){
+				  jQuery(this).stop();
+				},
+				function(){
+				  var offset = jQuery(this).offset();
+				  var residualSpace = offset.top + stripHeight;
+				  var residualTime = residualSpace/settings.travelocity;
+				  scrollnews(residualSpace, residualTime);
+				});			
+		});	
+};
+
+$(function(){
+    $("ul#ticker01").liScroll();
+});
+</script>
+<script>
+  jQuery.fn.liScroll = function(settings) {
+	settings = jQuery.extend({
+		travelocity: 50
+		}, settings);		
+		return this.each(function(){
+				var $strip = jQuery(this);
+				$strip.addClass("newsticker")
+				var stripHeight = 1;
+				$strip.find("li").each(function(i){
+					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+				});
+				var $mask = $strip.wrap("<div class='mask'></div>");
+				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+				$strip.height(stripHeight);			
+				var totalTravel = stripHeight;
+				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+				function scrollnews(spazio, tempo){
+				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
+				}
+				scrollnews(totalTravel, defTiming);				
+				$strip.hover(function(){
+				  jQuery(this).stop();
+				},
+				function(){
+				  var offset = jQuery(this).offset();
+				  var residualSpace = offset.top + stripHeight;
+				  var residualTime = residualSpace/settings.travelocity;
+				  scrollnews(residualSpace, residualTime);
+				});			
+		});	
+};
+
+
+$(function(){
+    $("ul#ticker01").liScroll();
+});
+</script>
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,263 @@
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Home || RBSSIET</title>
+  <link rel="stylesheet" href="/style.css">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+  <!-- JavaScript Bundle with Popper -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="/owlcarousel/owl.theme.default.min.css">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
+    />
+</head>
+
+<body>
+  <div class="container-fluid  bg-primary d-none d-sm-block">
+    <div class="row">
+      <div class="col-sm-4  py-2  d-flex justify-content-center align-items-center text-white">
+
+        <i class="fa-solid fa-phone px-3"></i>
+        <a href="tel: +91987654321" class="nav-link">+91 987654321</a>
+
+      </div>
+      <div class="col-sm-5 py-2  d-flex justify-content-center align-items-center text-white">
+        <i class="fa-solid fa-envelope px-3"></i>
+        <a href="mailto:support@rbssietrewari.ac.in" class="nav-link">support@rbssietrewari.ac.in</a>
+
+      </div>
+      <div class="col-sm-3 py-2  d-flex justify-content-center align-items-center text-white">
+        <i class="fa-brands fa-facebook px-2"></i>
+        <i class="fa-brands fa-twitter px-2"></i>
+        <i class="fa-brands fa-instagram px-2"></i>
+        <i class="fa-brands fa-linkedin px-2"></i>
+
+      </div>
+      
+      <!-- <div class="col-sm-3 py-2 d-none d-flex justify-content-center align-items-center text-white">
+        <i class="fa-solid fa-user-large px-3"></i>
+        <a href="#" class="nav-link">My Account </a>
+      </div> -->
+    </div>
+  </div>
+  <header class="">
+    <div class="container-fluid text-white text-center">
+      <div class="row">
+        <div class="col-2 d-flex justify-content-start">
+          <a href="#" class="logo">
+            <img src="/img/rbssiet new logo.png" class="img-fluid " width="60px" alt="">
+          </a>
+        </div>
+        <div class="col-9  d-flex justify-content-start flex-column align-items-center">
+       <div class="row">
+        <div class="col">
+          <h3 class="main-heading">
+            Rao Birender Singh <br class="d-block d-md-none"> State Institute Of Engineering And Technology, <br class="d-block d-md-none"> Zainabad, Rewari
+          </h3 >
+          
+        </div>
+       </div>  
+     
+      
+      <div class="row d-none d-lg-block">
+        <div class="col">
+          <ul class="navbar-nav text-uppercase d-flex flex-row justify-content-center rounded-3" style="font-size:15px ; font-weight: 600;">
+            <li class="nav-item white "><a href="#" class="nav-link ">Home</a></li>
+            <div class="dropdown">
+              <a
+                class="btn dropdown-toggle" style="color: rgb(255, 255, 255);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                About
+              </a>
+            
+              <ul class="dropdown-menu py-2" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <a
+                class="btn  dropdown-toggle" style="color: rgb(255, 253, 253);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Students
+              </a>
+            
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <a
+                class="btn  dropdown-toggle menu-color" style="color: rgb(255, 255, 255);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+               
+Faculty
+              </a>
+            
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <a
+                class="btn  dropdown-toggle" style="color: rgb(255, 255, 255);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+               
+
+              Admissions
+              </a>
+            
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <a
+                class="btn  dropdown-toggle" style="color: rgb(255, 255, 255);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+               
+
+              
+Media
+              </a>
+            
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <a
+                class="btn  dropdown-toggle" style="color: rgb(255, 255, 255);"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+               
+
+              
+Events/ Activities
+              </a>
+            
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+              </ul>
+            </div>
+              <li class="nav-item white " ><a href="#" class="nav-link ">
+                Tenders</a></li>
+              <div class="dropdown">
+                <a
+                  class="btn  dropdown-toggle" style="color: rgb(255, 255, 255);"
+                  href="#"
+                  role="button"
+                  id="dropdownMenuLink"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+News/ Notices/Announcements
+                </a>
+              
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <li><a class="dropdown-item py-3" href="#">Principal's Desk</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Placements</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Projects</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Luminaries</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Annual Report</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Mandatory Disclosure</a></li>
+                  <li><a class="dropdown-item py-3" href="#">RTI Act 2005</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Contact</a></li>
+                  <li><a class="dropdown-item py-3" href="#">Route To Institute</a></li>
+                </ul>
+              </div>
+              
+             
+        </div>
+      </div>
+    </div>
+    <div class="col-1 align-items-center bars d-flex d-block d-lg-none">
+      <i class="fa-solid fa-bars"></i>
+    </div>
+    </div>
+    
+  </header>
