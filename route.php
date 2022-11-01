397ca0ccd3019a8daa32ccce1856350114bbd620
Vimal Pandey <107113571+vimalpandey041@users.noreply.github.com>
2022-11-01 19:45:05 +0530

    new

--- route.php
+++ route.php
@@ -1,3 +1,121 @@
 <?php
 include("header.php");
 ?>
+<div class="container-fluid m-0 p-0 "   >
+    <img src="img/WAll.jpeg" width="100%" height="500px" style="object-fit:cover; ">
+    <div class="container-fluid position:absolute">
+        <!-- <div class="container" >
+
+
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Virtual tour</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Route to Institute</li>
+                </ol>
+            </nav>
+        </div>
+    </div> -->
+</div>
+<div class="container-fluid py-5">
+     <div class="row">
+ <div class="col-5 d-flex m-5">
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+</div>
+<div class="col-7 mx-5 ">
+<div class="conatiner-fluid">
+<h2>The world in one place</h2>
+<p>
+ There's so much going on that you could never leave campus and still find something new to do every day.
+</p>
+<p>
+Home to 15,000 students from more than 130 countries, our Colchester Campus is the largest of our three sites, making us one of the most internationally diverse campuses on the planet - we like to think of ourselves as the world in one place.
+
+Set within the 200-acre award-winning beautiful parkland - Wivenhoe Park and located two miles from the historic city centre of Colchester – England's oldest recorded development. Our Colchester Campus is also easily reached from London and Stansted Airport in under one hour.
+
+Whether you are planning to visit us at one of our Open Days, or coming to an Applicant day. Our campus conveniently located and easy to reach by car, train or bus.
+</p>
+
+</div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-title text-center">
+                    <h3>By Road</h3>
+                    <table border="2px solid-black"  >
+                    <tr>
+                        <td>Jhajjar City</td>
+                        <td>62.4km</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Gurugram</td>
+                        <td>84.2km</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Delhi</td>
+                        <td>111.2km</td>
+                        
+                    </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <h2 class="card-title text-center">By Train</h2>
+                <p>
+                Nearest Railway station to the Institute is Dahina railway station. Rewari railway station is well connected to Jhajjar Junction and Rohtak junction and Delhi junction
+                </p>
+                <table border="2px solid-black"  >
+                    <tr>
+                        <td>Jhajjar City</td>
+                        <td>62.4km</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Gurugram</td>
+                        <td>84.2km</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Delhi</td>
+                        <td>111.2km</td>
+                        
+                    </tr>
+                    </table>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-title text-center">
+                    <h3 >
+                        Airpot
+                    </h3>
+                </div>
+                <p>Indira Gandhi International Airport(T3)                58 KMs</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="conatiner-fluid px-0 py-5">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4208.613441400267!2d76.38870003330051!3d28.281738583527357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3912a13976302a8b%3A0x241b72adf48f6c8a!2sRao%20Birender%20Singh%20State%20Institute%20Of%20Engineering%20%26%20Technology%20%2C%20Dahina!5e0!3m2!1sen!2sin!4v1667300800286!5m2!1sen!2sin" width="100%" height="450" style="border:0; " allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+<?php
+include("footer.php");
+?>
