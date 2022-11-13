28031d64918b7df5daa274a3a778d695d5139013
Vimal Pandey <107113571+vimalpandey041@users.noreply.github.com>
2022-11-13 16:16:35 +0530

    new

---
+++ syllabus.php
@@ -0,0 +1,49 @@
+<?php
+include("header.php");
+?>
+
+    <link rel="stylesheet" href="syllabus.css">
+<div class="container-fluid m-0 p-0 "   >
+<nav aria-label="breadcrumb" class="bac">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
+                    <li class="breadcrumb-item"><a href="contact.php">Contact</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Syllabus</li>
+                </ol>
+            </nav>
+     <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="100%" height="350px" style="object-fit:cover;" alt="">
+
+            
+        </div>
+        <div class="col-lg-12 col-sm-12 col-xs-12 Start">Syllabi for respective departments</div>
+    
+<nav>
+  <ol class="main-menu">
+    <li class="main-item"><span class="home"></span></li>
+    <li class="main-item"><span class="about"></span></li>
+    <li class="main-item"><span class="widgets"></span>
+      <ol class="sub-menu">
+        <li class="sub-item item-left"><span class="small"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="standart"></span></li>
+        <li class="sub-item item-left"><span class="big"></span></li>
+      </ol>
+    </li>
+    <li class="main-item"><span class="kabobs"></span>
+      <ol class="sub-menu">
+        <li class="sub-item item-right"><span class="premium"></span></li>
+        <li class="sub-item item-right"><span class="combo"></span></li>
+        <li class="sub-item item-right"><span class="special"></span></li>
+      </ol>
+    </li>
+    <li class="main-item"><span class="contact"></span></li>
+  </ol>
+</nav>
+<a class="me" href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" target="_blank"></a>
+    <?php
+    include("footer.php");
+?>
