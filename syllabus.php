42dbd7ec88192a3f9293093fa545f8351f417303
Vimal Pandey <107113571+vimalpandey041@users.noreply.github.com>
2022-11-13 19:36:40 +0530

    new

--- syllabus.php
+++ syllabus.php
@@ -15,17 +15,17 @@ include("header.php");
 
             
         </div>
-        <div class="col-lg-12 col-sm-12 col-xs-12 Start">Syllabi for respective departments as per university guidelines</div>
-    <div class="container cse text-center py-5">
-    <button class="glow-on-hover" type="button"> Check out the latest syllabus</button>
+        <div class="col-lg-12 col-sm-12 col-xs-12 Start">Syllabi  as per university guidelines</div>
+    <div class="container cse text-center pt-4 py-3">
+    <button class="glow-on-hover" type="button"> Download the latest syllabus</button>
     </div>
 
 <div class="container  ">
 
-<a href="https://drive.google.com/file/d/1XR-7W5Zg92eWaNVxKNL8_GRhmZiaVfCD/view?usp=sharing" class="bt blue block py-4 ">Syllabus for CSE Semester 1 & 2</a>
-<a href="https://drive.google.com/file/d/19eF9lCEP2y7hktRYW26ShmN0UrkmfthI/view?usp=sharing" class="bt blue block  py-4 my-4">Syllabus for CSE Semester 3 & 4</a>
-<a href="https://drive.google.com/file/d/1DdKveEHefZX6kp7fjLasTt4Y2-xdNF8r/view?usp=sharing" class="bt blue block py-4 my-4">Syllabus for CSE Semester 5 & 6</a>
-<a href="https://drive.google.com/file/d/1L0wlroF6oqGfUbqB76-3DBmJlBTqMgTt/view?usp=sharing" class="bt blue block py-4 my-4">Syllabus for CSE Semester 7 & 8 </a>
+<a href="https://drive.google.com/file/d/1XR-7W5Zg92eWaNVxKNL8_GRhmZiaVfCD/view?usp=sharing" class="bt blue block py-4 mt-0 ">Syllabus for  Semester 1 & 2</a>
+<a href="https://drive.google.com/file/d/19eF9lCEP2y7hktRYW26ShmN0UrkmfthI/view?usp=sharing" class="bt blue block  py-4 my-4">Syllabus for Semester 3 & 4</a>
+<a href="https://drive.google.com/file/d/1DdKveEHefZX6kp7fjLasTt4Y2-xdNF8r/view?usp=sharing" class="bt blue block py-4 my-4">Syllabus for  Semester 5 & 6</a>
+<a href="https://drive.google.com/file/d/1L0wlroF6oqGfUbqB76-3DBmJlBTqMgTt/view?usp=sharing" class="bt blue block py-4 my-4">Syllabus for  Semester 7 & 8 </a>
 
 
 </div>
