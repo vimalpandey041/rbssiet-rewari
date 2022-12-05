b04df2a7b884bc86a90611ccff89942a2e5f7d45
Muskan Panwar <mysmileinfinite@gmail.com>
2022-12-05 21:09:41 +0530

    gallery

--- gallery.php
+++ gallery.php
@@ -1,25 +1,24 @@
 <?php
 include("header.php");
 ?>
-<<<<<<< HEAD
 
 <div class="banner_gallery_grid">
     <div class="banner items_4">
         <div class="picture_wrapper">
             <h4 class="title">Title 1</h4>
-            <img src="https://cdn.pixabay.com/photo/2022/10/14/08/03/earbuds-7520738_960_720.jpg" alt="">
+            <img src="img/Rbssiet Photos/43.jpeg" alt="">
         </div>
         <div class="picture_wrapper">
             <h4 class="title">Title 2</h4>
-            <img src="https://cdn.pixabay.com/photo/2022/05/29/03/40/goldfinch-7228334_960_720.jpg" alt="">
+            <img src="https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600" alt="">
         </div>
         <div class="picture_wrapper">
             <h4 class="title">Title 3</h4>
-            <img src="https://cdn.pixabay.com/photo/2022/10/31/18/44/spider-web-7560535_960_720.jpg" alt="">
+            <img src="https://images.pexels.com/photos/919884/pexels-photo-919884.jpeg?auto=compress&cs=tinysrgb&w=600" alt="">
         </div>
       <div class="picture_wrapper">
             <h4 class="title">Title 5</h4>
-            <img src="https://cdn.pixabay.com/photo/2022/01/26/20/11/aluminum-foil-6969822_960_720.jpg" alt="">
+            <img src="img/Rbssiet Photos/33.jpeg" alt="">
         </div>
     </div>
 </div>
@@ -45,93 +44,50 @@ include("header.php");
 </div>
 
 <!-- section 3 -->
-<body>
-  <div class="card-container">
+
+  <div class="cardm-container">
     <div class="cardm">
-      <p class="card-text">I</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1633977334114-3c912e72daef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="spooky jack-o-lantern">
+      <p class="cardm-text">I</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1633977334114-3c912e72daef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="spooky jack-o-lantern">
     </div>
     <div class="cardm">
-      <p class="card-text">hope</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1601299570740-380e5ae4daa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="hocus pocus decor">
+      <p class="cardm-text">hope</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1601299570740-380e5ae4daa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="hocus pocus decor">
     </div>
     <div class="cardm">
-      <p class="card-text">you</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1604149370100-2cf3be3bc845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="spider web">
+      <p class="cardm-text">you</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1604149370100-2cf3be3bc845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="spider web">
     </div>
     <div class="cardm">
-      <p class="card-text">have</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1574619151033-a9e1f81cae52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="spooky forest">
+      <p class="cardm-text">have</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1574619151033-a9e1f81cae52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="spooky forest">
     </div>
      <div class="cardm">
-       <p class="card-text">a</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1634184944338-7a5abb630f9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="jack-o-lantern with witch hat">
+       <p class="cardm-text">a</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1634184944338-7a5abb630f9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="jack-o-lantern with witch hat">
     </div>
     <div class="cardm">
-      <p class="card-text">safe</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1604186838309-c6715f0d3e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="dog in ghost costume">
+      <p class="cardm-text">safe</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1604186838309-c6715f0d3e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="dog in ghost costume">
     </div>
      <div class="cardm">
-       <p class="card-text">and</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1573306533578-12e23afbf2c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="jack-o-lantern scarecrow">
+       <p class="cardm-text">and</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1573306533578-12e23afbf2c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="jack-o-lantern scarecrow">
     </div>
     <div class="cardm">
-      <p class="card-text">spooky</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1598983870677-01e066a0b901?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="spooky skeleton screaming">
+      <p class="cardm-text">spooky</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1598983870677-01e066a0b901?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="spooky skeleton screaming">
     </div>
      <div class="cardm">
-       <p class="card-text">Halloween!</p>
-      <img class="card-img" src="https://images.unsplash.com/photo-1597739239353-50270a473397?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="spooky street at night">
+       <p class="cardm-text">Halloween!</p>
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1597739239353-50270a473397?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="spooky street at night">
     </div>
   </div>
-</body>
 
-=======
- <div class="x-container">
-        <div class="x-header">
-          <div class="xh-inner">
-            <div class="xh-content">
-              <h1 class="xh-h1">Hi, im Tibix.</h1>
-              <h2 class="xh-h2">and this is... my AI gallery</h2>
-              <span class="xh-text">Enjoy fresh and hand-picked images that i've created using Midjourney AI.
-                <br/>
-                <br/>
-                I will update this gallery with new images from time to time. If you enjoy what i'm creating, there's alot more on my Twitter.
-              </span>
-              <div class="x-socials">
-                <a target="_top" href="https://www.twitter.com/Tiibiix" class="x-social" style="margin-right: 40px;">
-                  <img class="x-social-img" src="https://assets.codepen.io/1692350/x_twitter.png">
-                  <span>Tibix</span>
-                </a>
-                <a target="_top" href="https://www.twitter.com/def_not_ai" class="x-social">
-                  <img class="x-social-img" src="https://assets.codepen.io/1692350/x_twitter.png">
-                  <span>Def not AI</span>
-                </a>
-              </div>
-            </div>
-            
-          </div>
-        </div>
+
+ 
       
-      
-        <div class="x-gallery">
-          <div class="xg-tag-filter">
-            <div class="xg-btn xg-btn-tag xg-btn-active" data-tag="all">Show All</div>
-            <div class="xg-btn xg-btn-tag" data-tag="characters">Characters</div>
-            <div class="xg-btn xg-btn-tag" data-tag="scenery">Scenery</div>
-            <div class="xg-btn xg-btn-tag" data-tag="abstract">Abstract</div>
-          </div>
-          <div class="xg-container">
-            <div class="xg-loader">
-              <div class="xg-loader-text">sorting the gallery...</div>
-            </div>
-            <div class="xg-img-preview">
-      
-            </div>
-          </div>
-        </div>
-      </div>  
->>>>>>> 4e6a4b3c3c06fb6549eb21c34a4ff574edf4c20a
+        
 <?php
 include("footer.php");
 ?>
