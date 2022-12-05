3889eed184845731437e3f48f793b838c528c159
Muskan Panwar <mysmileinfinite@gmail.com>
2022-12-05 21:58:26 +0530

    gallery section 4

--- gallery.php
+++ gallery.php
@@ -60,15 +60,16 @@ include("header.php");
     </div>
     <div class="cardm">
       <p class="cardm-text">have</p>
-      <img class="cardm-img" src="https://images.unsplash.com/photo-1574619151033-a9e1f81cae52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="spooky forest">
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1633977334114-3c912e72daef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="spooky jack-o-lantern">
+
     </div>
      <div class="cardm">
        <p class="cardm-text">a</p>
-      <img class="cardm-img" src="https://images.unsplash.com/photo-1634184944338-7a5abb630f9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="jack-o-lantern with witch hat">
+       <img class="cardm-img" src="https://images.unsplash.com/photo-1601299570740-380e5ae4daa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="hocus pocus decor">
     </div>
     <div class="cardm">
       <p class="cardm-text">safe</p>
-      <img class="cardm-img" src="https://images.unsplash.com/photo-1604186838309-c6715f0d3e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="dog in ghost costume">
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1604149370100-2cf3be3bc845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="spider web">
     </div>
      <div class="cardm">
        <p class="cardm-text">and</p>
@@ -76,7 +77,7 @@ include("header.php");
     </div>
     <div class="cardm">
       <p class="cardm-text">spooky</p>
-      <img class="cardm-img" src="https://images.unsplash.com/photo-1598983870677-01e066a0b901?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="spooky skeleton screaming">
+      <img class="cardm-img" src="https://images.unsplash.com/photo-1601299570740-380e5ae4daa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="hocus pocus decor">
     </div>
      <div class="cardm">
        <p class="cardm-text">Halloween!</p>
@@ -84,6 +85,212 @@ include("header.php");
     </div>
   </div>
 
+<!-- section 4 -->
+
+  <section class="portfolio-section" id="portfolio">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          
+        </div>
+      </div>
+      <div class="portfolio-menu mt-2 mb-4">
+        
+      </div>
+      <ul class="row portfolio-item">
+        <li class="mix dev col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1562813733-b31f71025d54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1562813733-b31f71025d54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix wp col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1520085601670-ee14aa5fa3e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1520085601670-ee14aa5fa3e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix dev col-xl-3 col-md-4 col-12 col-sm-6 pd ">
+          <img src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd ">
+          <img src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix wp col-xl-3 col-md-4 col-12 col-sm-6 pd ">
+          <img src="https://images.unsplash.com/photo-1581472723648-909f4851d4ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1581472723648-909f4851d4ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix dev col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1605379399843-5870eea9b74e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=898&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1605379399843-5870eea9b74e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=898&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix wp col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1555099962-4199c345e5dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+             
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1555099962-4199c345e5dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix dev col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1627398242454-45a1465c2479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+        
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1627398242454-45a1465c2479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="mix wp col-xl-3 col-md-4 col-12 col-sm-6 pd">
+          <img src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80" itemprop="thumbnail" alt="Image description" />
+          <div class="portfolio-overlay">
+            <div class="overlay-content">
+              <a data-fancybox="item" title="click to zoom-in" href="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80" data-size="1200x600">
+                <div class="magnify-icon">
+                  <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <!-- JS Links -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <!-- Mixitup -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.2.2/mixitup.min.js'></script>
+  <!-- fancybox -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js'></script>
+  <!-- Fancybox js -->
+  <script>
+    /*Downloaded from https://www.codeseek.co/ezra_siton/mixitup-fancybox3-JydYqm */
+    // 1. querySelector
+    var containerEl = document.querySelector(".portfolio-item");
+    // 2. Passing the configuration object inline
+    //https://www.kunkalabs.com/mixitup/docs/configuration-object/
+    var mixer = mixitup(containerEl, {
+      animation: {
+        effects: "fade translateZ(-100px)",
+        effectsIn: "fade translateY(-100%)",
+        easing: "cubic-bezier(0.645, 0.045, 0.355, 1)"
+      }
+    });
+    // fancybox insilaze & options //
+    $("[data-fancybox]").fancybox({
+      loop: true,
+      hash: true,
+      transitionEffect: "slide",
+      /* zoom VS next////////////////////
+      clickContent - i modify the deafult - now when you click on the image you go to the next image - i more like this approach than zoom on desktop (This idea was in the classic/first lightbox) */
+      clickContent: function(current, event) {
+        return current.type === "image" ? "next" : false;
+      }
+    });
+  </script>
 
  
       
